c0e964a5499a8331038d657298b433db685206f8
Patiience <lu.xu.daniel@gmail.com>
2024-03-09 22:00:01 -0500

    Added python forms for API and HTML Scraper

---
+++ src/api_scraper.py
@@ -0,0 +1,202 @@
+import requests
+from requests.auth import HTTPBasicAuth 
+import pandas as pd
+import time
+import os
+import math
+
+# Declare lists to store feature data
+repo_url= []
+repo_stars = []
+#repo_watches = []
+repo_wiki = []
+repo_open_issues = []
+repo_forks = []
+repo_last_update = []
+repo_size = []
+repo_created_date = []
+repo_last_push = []
+repo_language = []
+repo_discussions = []
+repo_pages = []
+repo_license = []
+repo_archived = []
+repo_projects = []
+repo_homepage = []
+repo_org = []
+repo_topics = []
+repo_ssh_url = []
+
+
+
+# Function used to scrape the data using the Github API
+def get_github_repo_info(search_filter, page_number):
+    api_url = f"https://api.github.com/search/repositories?q="+str(search_filter)+"&page="+str(page_number)+"&per_page=100"
+
+    headers = {
+    "Authorization": f"Bearer ",
+    "Accept": "application/vnd.github.v3+json"
+    }
+
+    # Get number of repos to be used to determine number of pages in the calling cell below
+    num_repos = 0
+
+    response = requests.get(api_url, headers=headers)
+
+    while response.status_code != 200:
+        print(f"Request failed on page {page_number}")
+        delay_seconds = 60  # default delay
+        time.sleep(delay_seconds)
+        response = requests.get(api_url, headers=headers)
+
+    #response = requests.get(api_url)
+    if response.status_code == 200:
+        # Parse the JSON response
+        repo_list = response.json()['items']
+        num_repos = response.json()['total_count']
+
+        for repo_info in repo_list:
+
+            # Extract and print relevant information
+            repo_url.append(repo_info.get("html_url", "URL not found"))
+            repo_stars.append(repo_info.get("stargazers_count", "Stargazers count not found"))
+            #repo_watches.append(repo_info.get("subscribers_count", "Watchers count not found"))
+            repo_wiki.append(repo_info.get("has_wiki", "Wiki not found"))
+            repo_open_issues.append(repo_info.get("open_issues_count", "Open issues count not found"))
+            repo_forks.append(repo_info.get("forks_count", "Forks count not found"))
+            repo_last_update.append(repo_info.get("updated_at", "Last update not found"))
+            repo_size.append(repo_info.get("size", "size not found"))
+            repo_created_date.append(repo_info.get("created_at", "Created date not found"))
+            repo_last_push.append(repo_info.get("pushed_at", "Last push not found"))
+            repo_language.append(repo_info.get("language", "Language not found"))
+            repo_discussions.append(repo_info.get("has_discussions", "Discussions not found"))
+            repo_pages.append(repo_info.get("has_pages", "Pages not found"))
+            repo_archived.append(repo_info.get("archived", "Archived not found"))
+            repo_projects.append(repo_info.get("has_projects", "Projects not found"))
+            repo_topics.append(len(repo_info.get("topics", "No Topics")))
+            repo_ssh_url.append(repo_info.get("ssh_url", "Projects not found"))
+            repo_org.append(repo_info['owner'].get("type", "No type"))
+
+            # Conditional statements are to avoid possible errors
+            license = repo_info.get("license", "None")
+            if license == "None" or license is None:
+                repo_license.append("None")
+            else:
+                repo_license.append(license["spdx_id"])
+
+
+            homepage = repo_info.get("homepage", "No Homepage")
+            if homepage is None or len(homepage) == 0:
+               repo_homepage.append("None")
+            else:
+                repo_homepage.append(homepage)
+
+
+    else:
+        # If the request was not successful, print an error message
+        print("Error:", response.status_code)
+        print("Response:", response.text)
+
+    return num_repos
+
+
+
+# Function used to facilitate scraping by changing search filters for number of stars, and some of them created date
+def get_projects(low, high):
+    # Variable for determining range of projects
+    decrement = 500
+    # While loop to go through each range from low to high
+    while high != low:
+        # Change ranges accordingly to get <1000 projects
+        if high == 400000:
+            decrement = 375000
+        elif high == 25000:
+            decrement = 5000
+        elif high == 15000:
+            decrement = 500
+        elif high == 5000:
+            decrement = 100
+        elif high == 3000 or high == 1000:
+            decrement = 10
+        elif high == 680:
+            decrement = 5
+        elif high == 500:
+            decrement = 1
+
+
+        # Search URL just in case => q=stars%3A120..120+created%3A2021-01-01..2021-12-31&
+
+        # Add the 'created:' parameter for <178 stars
+        if high <= 179:
+            decrement = 1
+            # Value of 9 goes down to year 2016
+            for i in range(9):
+                year = 2024 - i
+                created_date = "+created%3A" + str(year) + "-01-01.." + str(year) + "-12-31"
+                print(high-decrement, high-1, year, 1)
+                return_value = get_github_repo_info("stars%3A"+str(high-decrement)+'..'+str(high-1)+created_date, 1)
+                # For loop to run function to get features
+                for page_number in range(2,math.ceil(return_value/100)+1):
+                    print(high-decrement, high-1, page_number)
+                    return_value = get_github_repo_info("stars%3A"+str(high-decrement)+'..'+str(high-1)+created_date, page_number)
+            # One more request for all projects <=2015
+            created_date = "+created%3A<=2015-12-31"
+            print(high-decrement, high-1, 2015, 1)
+            return_value = get_github_repo_info("stars%3A"+str(high-decrement)+'..'+str(high-1)+created_date, 1)
+            for page_number in range(2,math.ceil(return_value/100)+1):
+                print(high-decrement, high-1, 2015, page_number)
+                return_value = get_github_repo_info("stars%3A"+str(high-decrement)+'..'+str(high-1)+created_date, page_number)
+        else:
+            print(high-decrement, high-1, 1)
+            return_value = get_github_repo_info("stars%3A"+str(high-decrement)+'..'+str(high-1), 1)
+            for page_number in range(2,math.ceil(return_value/100)+1):
+                # For loop to run function to get features
+                print(high-decrement, high-1, page_number)
+                return_value = get_github_repo_info("stars%3A"+str(high-decrement)+'..'+str(high-1), page_number)
+        high -= decrement
+
+
+
+# Call the above function to begin scraping
+get_projects(100,178)
+
+
+
+# Create pandas DataFrame to store the data
+projects_df = pd.DataFrame({'Project URL':repo_url,
+                            'Clone SSH URL':repo_ssh_url,
+                            'Organization':repo_org,
+                            'Homepage':repo_homepage,
+                            'Last Update':repo_last_update,
+                            'Last Push':repo_last_push,
+                            'Created Date':repo_created_date,
+                            'Archived':repo_archived,
+                            'Size':repo_size,
+                            'Number of Stars':repo_stars,
+                            #'Number of Watches':repo_watches,
+                            'Open Issues + Open Pull Requests':repo_open_issues,
+                            'Number of forks':repo_forks,
+                            'Has a Wiki':repo_wiki,
+                            'Has Discussions':repo_discussions,
+                            'Has Projects':repo_projects,
+                            'Has Pages':repo_pages,
+                            'License':repo_license,
+                            'Language':repo_language,
+                            'Topics': repo_topics})
+
+# For printing the data frame
+# projects_df
+
+
+# Export to Excel
+# CHANGE THE EXPORTED FILE NAME ACCORDINGLY
+try:
+    with pd.ExcelWriter(
+        "project_100..679.xlsx",
+        mode="a",
+        engine="openpyxl",
+        if_sheet_exists="overlay",
+    ) as writer:
+         projects_df.to_excel(writer,sheet_name="Sheet1", startrow=writer.sheets["Sheet1"].max_row, index = False,header= False)
+except FileNotFoundError:
+    projects_df.to_excel("project_100..679.xlsx", index=False)
---
+++ src/html_scraper.py
@@ -0,0 +1,275 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+import pandas as pd
+import time
+from bs4 import BeautifulSoup
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
+# Declare list to be used to store projects that failed to collect any information
+error_projects = []
+
+# Chrome options to be added
+chrome_options = Options()
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-renderer-backgrounding")
+chrome_options.add_argument("--disable-background-timer-throttling")
+chrome_options.add_argument("--disable-backgrounding-occluded-windows")
+chrome_options.add_argument("--disable-client-side-phishing-detection")
+chrome_options.add_argument("--disable-crash-reporter")
+chrome_options.add_argument("--disable-oopr-debug-crash-dump")
+chrome_options.add_argument("--no-crash-upload")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-low-res-tiling")
+chrome_options.add_argument("--log-level=3")
+chrome_options.add_argument("--silent")
+chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+
+
+# Declare function to scrape the features for each project
+def scrape_page(project_url):
+
+    project_features = []
+
+    print(project_url)
+    # Add url to list
+    project_features.append(project_url)
+
+    # Get the OWNER/REPO
+    project = project_url[19:]
+    print(project)
+
+    # Set up Web Driver
+    driver = webdriver.Chrome(options=chrome_options)
+
+    driver.get(project_url)
+
+    # Get number of watches and sponsors
+    # Wait for the document to be in 'complete' state
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.TAG_NAME, 'footer'))
+    )
+
+    # Parse HTML
+    # Get number of watches and sponsered?
+    html = driver.page_source
+    soup = BeautifulSoup(html,"html.parser")
+
+    num_watches = soup.find(href=f"/{project}/watchers").find("strong").text
+
+    creator = project.split('/')[0]
+    sponsored = "Yes" if soup.find(href=f"/sponsors/{creator}") != None else "No"
+
+    project_features.append(num_watches)
+    project_features.append(sponsored)
+
+    # Issues
+    #thread = threading.Thread(name=watch_sponsors,target=issues(project_url, project, driver))
+    #thread.start()
+    issue_url = project_url + "/issues"
+    driver.get(issue_url)
+
+    # Wait for the document to be in 'complete' state
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.TAG_NAME, 'footer'))
+    )
+
+    html = driver.page_source
+    soup = BeautifulSoup(html,"html.parser")
+
+    open_issues = None if soup.find(href=f"/{project}/issues?q=is%3Aopen+is%3Aissue") == None else soup.find(href=f"/{project}/issues?q=is%3Aopen+is%3Aissue").text.split()[0]
+    closed_issues = None if soup.find(href=f"/{project}/issues?q=is%3Aissue+is%3Aclosed") == None else soup.find(href=f"/{project}/issues?q=is%3Aissue+is%3Aclosed").text.split()[0]
+    num_labels = None if soup.find(href=f"/{project}/labels") == None else soup.find(href=f"/{project}/labels").find("span").text
+    num_milestones = None if soup.find(href=f"/{project}/milestones") == None else soup.find(href=f"/{project}/milestones").find("span").text
+
+
+    project_features.append(open_issues)
+    project_features.append(closed_issues)
+    project_features.append(num_labels)
+    project_features.append(num_milestones)
+
+    print(f"Project:{project_url}, Open issues: {open_issues}, Closed issues: {closed_issues}")
+
+    # Pull Requests
+    pull_url = project_url + "/pulls"
+    driver.get(pull_url)
+
+    # Wait for the document to be in 'complete' state
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.TAG_NAME, 'footer'))
+    )
+
+    html = driver.page_source
+    soup = BeautifulSoup(html,"html.parser")
+
+    #print(f"Getting prs: {project}")
+    #open_prs = soup.find(href=f"/{project}/pulls?q=is%3Aopen+is%3Apr")
+    #if open_prs != None:
+     #   open_prs = open_prs.text.split()[0]
+    #else:
+     #   count = 0
+      #  while count < 4 and open_prs == None:
+       #     driver.get(pull_url)
+        #    # Wait for the document to be in 'complete' state
+         #   WebDriverWait(driver, 10).until(
+         #       EC.visibility_of_element_located((By.TAG_NAME, 'body'))
+          #  )
+          #  html = driver.page_source
+           # soup = BeautifulSoup(html, "html.parser")
+           # open_prs = soup.find(href=f"/{project}/pulls?q=is%3Aopen+is%3Apr")
+           # count += 1
+       # if open_prs == None:
+       #     return [None, project_url]
+       # else:
+        #    open_prs = open_prs.text.split()[0]
+    #closed_prs = soup.find(href=f"/{project}/pulls?q=is%3Apr+is%3Aclosed").text.split()[0]
+
+    #print(f"Getting prs: {project} Open={open_prs} Closed={closed_prs}")
+
+    #project_features.append(open_prs)
+    #project_features.append(closed_prs)
+
+    # Number of Workflow Runs
+    workflow_url = project_url + "/actions"
+    driver.get(workflow_url)
+
+    # Wait for the document to be in 'complete' state
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.TAG_NAME, 'footer'))
+    )
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    workflow = soup.find(lambda tag: tag.name == 'strong' and 'workflow runs' in tag.get_text())
+    if workflow != None:
+        workflow = workflow.text.split()[0]
+    print(f"workflow: {workflow}")
+
+    project_features.append(workflow)
+
+    # Number of Dependent Repos
+    dependent_url = project_url + "/network/dependents"
+    driver.get(dependent_url)
+
+    # Wait for the document to be in 'complete' state
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.TAG_NAME, 'footer'))
+    )
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    dependents = soup.find('a', class_='btn-link selected')
+    if dependents != None:
+        dependents = dependents.text.split()[0]
+
+    print(f"dependents: {dependents}")
+
+    project_features.append(dependents)
+
+    # Verified Repo Owner
+    owner_url = f"https://github.com/{creator}"
+    print(owner_url)
+    driver.get(owner_url)
+
+    # Wait for the document to be in 'complete' state
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.TAG_NAME, 'footer'))
+    )
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    verified = soup.find('summary', {'title': 'Label: Verified'})
+    if verified != None:
+        verified = verified.text.split()[0]
+
+    print(verified)
+    project_features.append(verified)
+
+    # Number of Owner Followers
+    #followers = soup.find('a', class_='Link--secondary no-underline no-wrap')
+    #if followers != None:
+      #  followers = followers.text.split()[0]
+   # else:
+      #  count = 0
+       # while count < 4 and followers == None:
+        #    driver.get(owner_url)
+         #   # Wait for the document to be in 'complete' state
+          #  WebDriverWait(driver, 10).until(
+           #     EC.visibility_of_element_located((By.TAG_NAME, 'body'))
+            #)
+           # html = driver.page_source
+           # soup = BeautifulSoup(html, "html.parser")
+           # followers = soup.find('a', class_='Link--secondary no-underline no-wrap')
+           # count += 1
+        #if followers == None:
+       #     return [None, project_url]
+        #else:
+         #   followers = followers.text.split()[0]
+    #print(f"followers: {followers}")
+
+    #project_features.append(followers)
+
+    # Clean close the Web Session and window(s)
+    driver.quit()
+
+    return project_features
+
+
+# RUNNING CODE
+# Read in the excel file consisting of projects to scrape
+project_list = pd.read_excel('project_5000Up.xlsx')
+project_list = project_list['Project URL'].tolist()
+project_list = project_list[:100]
+
+
+# Create list to store features scraped and execute the function above to scrape the features
+projects = []
+low = 0
+high = 10
+with ThreadPoolExecutor(max_workers=10) as p:
+    while high <= len(project_list):
+        features = p.map(scrape_page, project_list[low:high])
+        print(high)
+        for f in features:
+            if f[0] != None:
+                projects.append(f)
+            else:
+                error_projects.append(f[1])
+
+        low += 10
+        high += 10
+        time.sleep(2)
+
+# Create pandas DataFrame to store the data
+projects_df = pd.DataFrame(projects, columns=['Project URL',
+                                              'Number of Watches',
+                                              'Sponsored',
+                                              'Open Issues',
+                                              'Closed Issues',
+                                              'Number of Labels',
+                                              'Number of Milestones',
+                                              #'Open Pull Requests',
+                                              #'Closed Pull Requests',
+                                              'Number of Workflow Runs',
+                                              'Number of Dependents',
+                                              'Verified Owner',
+                                              #'Followers of Owner'
+                                             ])
+# For printing data frame
+# projects_df
+
+
+# Export to excel file
+# try: with pd.ExcelWriter( "project_HTMLfeatures.xlsx", mode="a", engine="openpyxl", if_sheet_exists="overlay", ) as writer: projects_df.to_excel(writer,sheet_name="Sheet1", startrow=writer.sheets["Sheet1"].max_row, index = False,header= False) except FileNotFoundError: projects_df.to_excel("project_HTMLfeatures.xlsx", index=False)
