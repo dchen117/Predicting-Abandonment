0c03983b3d2527525c565a2fe049fe1cbb7fd40b
Patiience <lu.xu.daniel@gmail.com>
2024-04-15 22:32:28 -0400

    Added a lil to main.py

--- src/main/main.py
+++ src/main/main.py
@@ -1,29 +1,58 @@
+# Import the python scripts and another libraries
 import api_modified as api_m
 import html_scraper as html
 import pandas as pd
 import argparse
 from pathlib import Path
+import subprocess
 
 # Adding ArgumentParse object with description
 parser = argparse.ArgumentParser(description='Scrapes features from Github projects.')
 
-# Adding command-line arguments and options
-parser.add_argument('access_token', help='Github access token for authorization.')
-parser.add_argument('export_file', help='Name of excel file that stores the collected features.')
-parser.add_argument('import_file', help='Name of excel file with list of projects to be collected. The list should be in the first column of the excel file.')
+# Adding command-line argument --mode
+parser.add_argument('--mode', choices=['scrape', 'rescrape', 'subscrape'], help='Specify mode for scraping: scrape, rescrape, subscrape.')
 
 # Parse command-line arguments
 args = parser.parse_args()
 
-# Access arguments
-access_token = args.access_token
-export_file = args.export_file
-import_file = args.import_file
+# Access mode specified 
+mode = args.mode
+
+# Execute based on mode
+if mode == 'scrape':
+  # Add and retrieve arguments for this mode
+  parser.add_argument('access_token', help='Github access token for authorization.')
+  parser.add_argument('export_file', help='Name of excel file that stores the collected features.')
+  parser.add_argument('import_file', help='Name of excel file with list of projects to be collected. The list should be in the first column of the excel file.')
+
+  # Access arguments
+  access_token = args.access_token
+  export_file = args.export_file
+  import_file = args.import_file
+
+elif mode == 'rescrape':
+  # Add and retrieve arguments for this mode
+  parser.add_argument('access_token', help='Github access token for authorization.')
+  parser.add_argument('export_file', help='Name of excel file that stores the collected features.')
+  parser.add_argument('import_file', help='Name of excel file with list of projects to be collected. The list should be in the first column of the excel file.')
+
+  # Access arguments
+  access_token = args.access_token
+  export_file = args.export_file
+  import_file = args.import_file
+
+elif mode == 'subscrape':
 
 # Scraping features using html and api scrapers
 html.scrape_project('https://github.com/freeCodeCamp/freeCodeCamp')
 api_m.scrape_project('https://github.com/freeCodeCamp/freeCodeCamp', access_token)
 api_m.collect_sbom('https://github.com/freeCodeCamp/freeCodeCamp', access_token)
+
+# Run the bash script scraper, using subprocess.run()
+#command = f"./clone_scraper.sh {import_file_for_list_of_projects} {export_file_for_bash_info}"
+#result = subprocess.run(command, shell=True, capture_output=True, text=True)
+# Print the output
+# print(result.stdout)
 
 # Converting features to pandas dataframe
 api_df = api_m.convertToDataFrame()
